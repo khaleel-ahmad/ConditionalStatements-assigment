f16ce5ffc719755d1dab5d5086d8925295b7161e
khaleel <khaleelahmad303@gmail.com>
2022-11-23 00:43:19 +0200

    Sorry for delay

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+print("Hello Friend")
+
+name = input("input your name: ")
+age = input("input your age: ")
+address = input("input your address: ")
+if name.isalpha() == True:
+    if age.isdigit() == True:
+        print(
+            "Hello Mr\Ms {name} age is {age} and located in {address} thanks for being one of our coomuunity")
+    else:
+        print("please enter a number of age")
+else:
+    print("please enter a name not a number")
